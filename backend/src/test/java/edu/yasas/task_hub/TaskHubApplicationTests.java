d5fe6f0e232c41ab6cdead3b179aa8a02b6335f4
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-18 21:20:29 +0530

    backend init

---
+++ backend/src/test/java/edu/yasas/task_hub/TaskHubApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yasas.task_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
