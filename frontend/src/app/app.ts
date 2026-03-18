983bb66f94769f29c797705be7829be88c08fea2
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-18 20:30:42 +0530

    frontend / login form component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginForm } from './components/login-form/login-form';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ],
+  imports: [RouterOutlet, LoginForm],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
