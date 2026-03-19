49d14f0254307c752f566a9fe488b9fb3d438cdd
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-19 10:50:36 +0530

    frontend/ login form input validation

--- frontend/src/app/components/login-form/login-form.ts
+++ frontend/src/app/components/login-form/login-form.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-login-form',
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule,FontAwesomeModule],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css',
 })
-export class LoginForm {}
+export class LoginForm {
+
+  isPasswordShowing: boolean = false;
+  eyeIcon = faEye;
+  closedEyeIcon = faEyeSlash;
+
+  loginForm = new FormGroup({
+    email: new FormControl('',[Validators.email,Validators.required]),
+    password: new FormControl('',[Validators.required])
+  });
+
+  togglePassword() {
+    this.isPasswordShowing = !this.isPasswordShowing;
+  }
+
+}
