c2b58e5078925cfccc348156b321cb57edc9f81e
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-19 07:49:51 +0530

    frontend/ register form ui added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { RegisterForm } from './components/register-form/register-form';
+import { LoginForm } from './components/login-form/login-form';
+
+export const routes: Routes = [
+    {path: '', component: LoginForm},
+    {path:'register', component:RegisterForm}
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,7 +5,7 @@ import { LoginForm } from './components/login-form/login-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginForm],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
