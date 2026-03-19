c32257b0e7221f216cb366e3a56ea6ec249abc8d
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-19 09:14:51 +0530

    frontend/ register form input validation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { Routes } from '@angular/router';
 import { RegisterForm } from './components/register-form/register-form';
 import { LoginForm } from './components/login-form/login-form';
 
+
 export const routes: Routes = [
     {path: '', component: LoginForm},
-    {path:'register', component:RegisterForm}
+    {path:'register', component:RegisterForm},
+   
 ];
--- frontend/src/app/components/register-form/register-form.ts
+++ frontend/src/app/components/register-form/register-form.ts
@@ -1,9 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule,FontAwesomeModule ],
   templateUrl: './register-form.html',
   styleUrl: './register-form.css',
 })
-export class RegisterForm {}
+export class RegisterForm {
+
+  isPasswordShowing: boolean = false;
+  eyeIcon = faEye;
+  closedEyeIcon = faEyeSlash;
+
+  registerForm = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email]),
+    username: new FormControl('',[Validators.required,Validators.min(3)]),
+    password: new FormControl('',[Validators.required,Validators.min(8)])
+  })
+
+  clearForm(){
+    this.registerForm.reset();
+  }
+
+  submitForm(){
+    console.log(this.registerForm.value);
+  }
+
+  togglePassword() {
+    this.isPasswordShowing = !this.isPasswordShowing;
+  }
+
+}
